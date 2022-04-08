6305a0f91a6595621fea19b22d30df5c9c70c807
rottaj <jackrotta12@gmail.com>
2022-04-07 23:59:31 -0400

    Added Txn Modal

--- app/src/Components/TransactionModal.tsx
+++ app/src/Components/TransactionModal.tsx
@@ -1,6 +1,5 @@
 import { ethers } from 'ethers';
-import * as crypto from 'crypto';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import {
     Box,
     Heading,
@@ -14,6 +13,7 @@ import {
     ModalOverlay,
     ModalContent
 } from "@chakra-ui/react";
+import { AccountContext } from '../contexts';
 
 type TransactionModalProps = {
     isOpen: any;
@@ -22,6 +22,18 @@ type TransactionModalProps = {
 }
 
 const TransactionModal = ({isOpen, onOpen, onClose}: TransactionModalProps) => {
+
+    const [ recipientAddress, setRecipientAddress ]: any = useState()
+    const { etherBalance }: any = useContext(AccountContext);
+
+    const checkValidEthereumAddress = (address: string) => {
+        try {
+            if (ethers.utils.getAddress(address)) {
+                setRecipientAddress(address)
+            } 
+        } catch(error) {} 
+    }
+
     return (
          <Modal
             isOpen={isOpen}
@@ -38,20 +50,57 @@ const TransactionModal = ({isOpen, onOpen, onClose}: TransactionModalProps) => {
                 border="1px solid black"
             >
                 <ModalBody>
-                    <Box>
-                        <Flex pt="60px">
-                            <Heading pl="170px">Send to</Heading>
-                            <Text 
-                                marginLeft="auto"
-                                pr="20px" pt="7px"
-                            >Cancel</Text>
-                        </Flex>
-                        <Input
-                            width="90%"
-                            height="30px"
-                            placeholder="Search, public address (0x), or ENS"
-                        ></Input>
-                    </Box>
+                    {recipientAddress ? 
+                        <Box
+                            textAlign="center"
+                        >
+
+                            <Heading pt="60px">Send</Heading>
+                            <Box 
+                                mx="3%"
+                                border="1px solid black"
+                                borderRadius="10px"
+                            >
+                                <Text>{recipientAddress}</Text>
+                            </Box>
+
+                            <Box>
+                                <Box 
+                                    mx="20%"
+                                    borderRadius="10px"
+                                    border="1px solid black"
+                                    textAlign="left"
+                                    pl="15px"
+                                >
+                                    <Text margin="0">ETH</Text>
+                                    <Text margin="0">Balance: {etherBalance}</Text>
+                                </Box>
+                                <Box
+                                    mx="20%"
+                                    borderRadius="10px"
+                                    border="1px solid black"
+                                >
+                                    <Input placeholder="0"></Input>
+                                </Box>
+                            </Box>
+                        </Box>
+                    :
+                        <Box>
+                            <Flex pt="60px">
+                                <Heading pl="170px">Send to</Heading>
+                                <Text 
+                                    marginLeft="auto"
+                                    pr="20px" pt="7px"
+                                >Cancel</Text>
+                            </Flex>
+                            <Input
+                                width="90%"
+                                height="30px"
+                                placeholder="Search, public address (0x), or ENS"
+                                onChange={(e) => checkValidEthereumAddress(e.target.value)}
+                            ></Input>
+                        </Box>
+                    }
                 </ModalBody>
             </ModalContent>
         </Modal>       
